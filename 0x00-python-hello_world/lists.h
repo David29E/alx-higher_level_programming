49cb623407aea716d471082eb03ee56ed070d8e9
David29E <dawitalemayehu234@gmailcom>
2023-09-04 06:26:55 -0700

    lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -7,6 +7,9 @@
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node 
+ *
+ * Description: singly linked list node stracture
+ *
  */
 typedef struct listint_s
 {
